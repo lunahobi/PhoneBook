07525bc790f335eda172c6b1098b690e9f1c4135
lunahobi <muradyan.lusine.05@mail.ru>
2023-06-23 13:11:32 +0500

    Добавлен метод showContacts

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -2,13 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-    private static Map<String, Human> directory = new HashMap<>();
-    public static void addEntry(String phone, Human fio) {
+    private static Map<String, Human> directory;
+    public PhoneBook() {
+        this.directory = new HashMap<String, Human>();
+    }
+    public void addEntry(String phone, Human fio) {
         directory.put(phone, fio);
         System.out.println("Абонент успешно добавлен.");
     }
 
-    public static void deleteEntry(String phoneNumber) {
+    public void deleteEntry(String phoneNumber) {
         if(directory.containsKey(phoneNumber)) {
             String name = directory.get(phoneNumber).last_name + " " + directory.get(phoneNumber).first_name + " " + directory.get(phoneNumber).second_name;
             directory.remove(phoneNumber);
@@ -18,13 +21,21 @@ public class PhoneBook {
         }
     }
 
-    public static void searchByPhoneNumber(String phoneNumber) {
-
+    public void searchByPhoneNumber(String phoneNumber) {
         if (directory.containsKey(phoneNumber)) {
             String last_name = directory.get(phoneNumber).last_name;
             System.out.println("Фамилия абонента номера " + phoneNumber + " это " + last_name);
         } else {
             System.out.println("Не найден абонент с номером телефона " + phoneNumber);
+        }
+    }
+    public void showContacts() {
+        if (this.directory.isEmpty()) {
+            System.out.println("Телефонный справочник пустой");
+        } else {
+            for (Map.Entry<String, Human> entry : this.directory.entrySet()) {
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
         }
     }
 }
